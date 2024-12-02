edb7a1e31c97e154041616507384bc5966083533
John Hart <john@johnhart96.co.uk>
2024-12-02 15:13:57 +0000

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,22 @@
+  GNU nano 7.2                                       config.example.php
+<?php
+/*
+* Project: DBDNS
+* Author: John Hart
+* Licence: Apache 2.0
+*/
+
+// General
+define( "SERVER_NAME" , "ns1" );
+define( "SERVER_TYPE" , "MASTER" );
+define( "ZONE_DIR" , "/etc/bind/zones" );
+define( "ADMIN_EMAIL" , "YOUR EMAIL HERE" );
+
+// Database
+define( "DB_HOST" , "" );
+define( "DB_PORT" , 3306 );
+define( "DB_USERNAME" , "" );
+define( "DB_PASSWORD" , "" );
+define( "DB_NAME" , "dbdns" );
+
+?>
